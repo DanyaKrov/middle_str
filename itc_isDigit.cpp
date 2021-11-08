52c07500f23f6eadebd62d84a4a21252ad488f54
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-11-08 12:13:18 +0300

    1 функция

---
+++ itc_isDigit.cpp
@@ -0,0 +1,6 @@
+#include "middle_str.h"
+
+bool itc_isDigit(unsigned char c)
+{
+    return (8 <= c && c <= 57);
+}
---
+++ middle_str.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+#ifndef MIDDLE_STR_H_INCLUDED
+#define MIDDLE_STR_H_INCLUDED
+
+
+bool itc_isDigit(unsigned char c);
+#endif // MIDDLE_STR_H_INCLUDED
