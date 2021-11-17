b68d39a10e6376a9baaa7e5a2b3a85afe0d61ef9
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-11-17 18:28:53 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -9,8 +9,16 @@ using namespace std;
 
 bool itc_isDigit(unsigned char c);
 unsigned char itc_toUpper(unsigned char c);
-unsigned char itc_registr(unsigned char c);
+unsigned char itc_changeCase(unsigned char c);
 bool itc_Compare(string S1, string S2);
 long long itc_len(string str);
-int itc_kol_slov(string str);
+int itc_countWords(string str);
+string itc_maxCharWord(string str);
+char itc_sameChar(string str);
+bool itc_isFirstInSecond(string s1, string s2);
+string itc_Cezar(string str, int k);
+string itc_rmFreeSpace(string str);
+bool itc_isIp(string str);
+string itc_DecToBin(string str);
+string itc_decToBase(int num, int base);
 #endif // MIDDLE_STR_H_INCLUDED
