4a8d082f18699948090ff2cc2ef4a65499737a65
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-11-17 19:37:05 +0300

    8

---
+++ itc_isFirstInSecond.cpp
@@ -0,0 +1,22 @@
+#include "middle_str.h"
+
+
+bool itc_isFirstInSecond(string s1, string s2)
+{
+    bool statement = false;
+    long long length = 0;
+    for (long long i = 0; s2[i] != '\0'; i++)
+    {
+        if (s1[length] == s2[i])
+        {
+            length ++;
+        }
+        else
+        {
+            statement = (length == itc_len(s1));
+            length = 0;
+        }
+    }
+    statement = (length == itc_len(s1));
+    return statement;
+}
