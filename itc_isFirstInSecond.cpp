058041abaad59d645e687d77aa3356c7cbef2a29
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-11-18 22:56:49 +0300

    Update itc_isFirstInSecond.cpp

--- itc_isFirstInSecond.cpp
+++ itc_isFirstInSecond.cpp
@@ -29,28 +29,26 @@ long long itc_find_str(string str1, string str2)
 }
 bool itc_isFirstInSecond(string s1, string s2)
 {
-    if(s1 == "" || s2 == "")
+    if (s1 == "" && s2 == "")
         return false;
-    if(itc_len(s1) > itc_len(s2))
+    string tmp = s1;
+	s1 = s2, s2 = tmp;
+	int n1 = itc_len(s1);
+	int n2 = itc_len(s2);
+	bool statement = false;
+	if (n1 < n2)
         return false;
-    long long l1 = itc_len(s1), l2 = itc_len(s2), ans = 0;
-    bool statement = true;
-    for (int _ = 0; _ < l2; _++)
-    {
-        if(s1[0] == s2[_])
-        {
-            while (11 > ans)
-            {
-                if (!(s2[_ + ans] == s1[ans]))
-                {
-                    statement = 0;
-                }
-                ans ++;
-            }
-            if (statement)
-                return statement;
-        }
-        statement = true;
-    }
-    return false;
+	for (int i = 0; i < n1-n2+1; i++){
+		bool statement = false;
+		if (s1[i] == s2[0]) {
+			statement = true;
+			for (int j = 1; j < n2; j++)
+				if (s1[i + j] != s2[j])
+                    statement = false;
+		}
+
+		if (statement)
+            return statement;
+	}
+	return false;
 }
