70b9b620f0e0947ea5bd229ab9b3c18724fc3e89
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-11-18 22:52:20 +0300

    Update itc_isFirstInSecond.cpp

--- itc_isFirstInSecond.cpp
+++ itc_isFirstInSecond.cpp
@@ -27,37 +27,6 @@ long long itc_find_str(string str1, string str2)
     return -1;
     }
 }
-
-#include "middle_str.h"
-
-long long itc_find_str(string str1, string str2)
-{
-    if (itc_len(str2) > itc_len(str1))
-        return -1;
-    else
-    {
-        int answer = 1;
-    for (int i = 0; i < itc_len(str1); i++)
-    {
-        if (str1[i] == str2[0] && itc_len(str1) - i >= itc_len(str2))
-        {
-                for (int j = 1; j < itc_len(str2); j++)
-                {
-                    if (str2[j] == str1[i + j])
-                        answer++;
-                    else
-                    {
-                        answer = 1;
-                    }
-                }
-                if (answer == itc_len(str2))
-                    return i;
-        }
-    }
-    return -1;
-    }
-}
-
 bool itc_isFirstInSecond(string s1, string s2)
 {
     if(s1 == "" || s2 == "")
