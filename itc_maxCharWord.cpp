5f6314bab55ed132f8286b7c94013e23e762f96d
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-11-18 21:31:29 +0300

    Update itc_maxCharWord.cpp

--- itc_maxCharWord.cpp
+++ itc_maxCharWord.cpp
@@ -2,11 +2,9 @@
 
 string itc_maxCharWord(string str)
 {
-    bool issp = false;
     long long max = -1, length = 0;
     string word = "";
     string ans = "";
-    int kol = 0;
     if (str == "")
         return "error";
     for (long long i = 0; str[i] != '\0'; i++)
@@ -18,24 +16,18 @@ string itc_maxCharWord(string str)
         }
         else
         {
-            if (max < length && str[i] == ' '){
-                issp = true;
-                ans = word;
+            if (max < length)
+            {
                 max = length;
+                ans = word;
             }
-            if (length > 0)
-                kol++;
             length = 0;
             word = "";
         }
     }
-    if (!issp)
-        return "error";
-    if (length > 0)
-        kol++;
-    if (kol < 2)
-        return "error";
     if (max < length)
         ans = word;
+    if (itc_countWords(str) < 2)
+        return "error";
     return ans;
 }
