40af94353e454cc17cb233c9bdc3912881d4bc26
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-11-18 23:00:12 +0300

    Update itc_isFirstInSecond.cpp

--- itc_isFirstInSecond.cpp
+++ itc_isFirstInSecond.cpp
@@ -40,10 +40,11 @@ bool itc_isFirstInSecond(string s1, string s2)
         return false;
 	for (int i = 0; i < n1-n2+1; i++){
 		bool statement = false;
-		if (s1[i] == s2[0]) {
+		if (s1[i] == s2[0]) 
+        {
 			statement = true;
-			for (int j = 1; j < n2; j++)
-				if (s1[i + j] != s2[j])
+			for (int _ = 1; _ < n2; _++)
+				if (s1[i + _] != s2[_])
                     statement = false;
 		}
 
