2e3a7f80f0b6bd78a129a36aac08756bd46696ed
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-11-18 10:10:21 +0300

    Update itc_compare.cpp

--- itc_compare.cpp
+++ itc_compare.cpp
@@ -2,6 +2,8 @@
 
 bool itc_compare(string S1, string S2)
 {
+    if (S1 == "" || S2 == "")
+        return false;
     long long len1 = itc_len(S1), len2 = itc_len(S2);
     if (len1 != len2)
         return false;
